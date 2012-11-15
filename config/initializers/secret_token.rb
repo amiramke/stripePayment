37ea4a9e9695905b8e48d9ef2163f533d6f7f554
Apprentice <apprentice@devbootcamp.com>
2012-11-14 17:56:10 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StripePayment::Application.config.secret_token = 'dcdb02a35640df586dddb7fb6b7aec07eb37d6a66a22fcf0b0f07b3f63fb2b7931eff75341d3b1e089b7cf35f551a7f7e8d3100adb3497860ba7208082f1034e'
