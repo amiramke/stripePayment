7be9a8bd80267bb486109fe1ef13bac6ec1c6ffd
Apprentice <apprentice@devbootcamp.com>
2012-11-14 21:34:01 -0800

    Add subscription

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -17,16 +17,25 @@ class PaymentsController < ApplicationController
     logger.info(params)
     token = params[:stripeToken]
 
-    # create the charge on Stripe's servers - this will charge the user's card
-    charge = Stripe::Charge.create(
-      :amount => 1200000, # amount in cents, again
-      :currency => "usd",
-      :card => token,
-      :description => "payinguser@example.com"
-    )
+    if params[:subscribe]
+      # create a Customer
+      customer = Stripe::Customer.create(
+        :card => token,
+        :plan => "basic",
+        :email => "payinguser@example.com"
+      )
+    else
+      # create the charge on Stripe's servers - this will charge the user's card
+      charge = Stripe::Charge.create(
+        :amount => 1200000, # amount in cents, again
+        :currency => "usd",
+        :card => token,
+        :description => "payinguser@example.com"
+      )
+    end
+
     logger.info charge
     @charge = charge
-    @email  = params[:email]
     flash[:notice] = "You've sent money!"
     redirect_to payment_path(1)
   end
